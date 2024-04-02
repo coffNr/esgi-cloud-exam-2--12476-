507882b2a7db764393e32b1cf26909da04d0b14c
Oceane <megacofie@gmail.com>
2024-04-02 15:18:57 +0200

    bdd update

--- db.client.js
+++ db.client.js
@@ -1,23 +1,31 @@
-const { Sequelize } = require('sequelize')
 
-// database
+const { Sequelize } = require('sequelize');
+
+// Connexion à la base de données PostgreSQL
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  'esgi_cloud_exam_2_12476', // Nom de la base de données
+  'esgi_cloud_exam_2_12476_user', // Nom d'utilisateur
+  '', // Mot de passe (laissez vide si aucun)
   {
+    host: 'dpg-co60964f7o1s73a9362g-a', // Hôte de la base de données
+    port: 5432, // Port de la base de données
+    dialect: 'postgres', // Dialecte de la base de données
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
-  },
+  }
 );
 
-// authentication and synchronization
+// Vérification de la connexion à la base de données
 sequelize.authenticate()
   .then(() => {
-    sequelize.sync().catch(() => console.log("Cannot sync the database"));
+    console.log('Connexion à la base de données réussie.');
   })
-  .catch(() => console.log("Cannot connect to database, please check environment credentials"));
+  .catch(err => {
+    console.error('Impossible de se connecter à la base de données :', err);
+  });
 
 module.exports = sequelize;
